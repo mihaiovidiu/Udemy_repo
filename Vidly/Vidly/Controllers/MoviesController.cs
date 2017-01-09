0984b6fb5360bfca2a8fdd67f37e5c6b3d52a7d6
Mihai Gaiseanu <Mihai Gaiseanu>
2017-01-09 17:17:15 +0200

    finished section 1

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        // Returns a random movie
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name = "Shreck!"};
+            return View(movie);
+        }
+    }
+}
