c23d7bee861e6ab8a372d7411043b82c7d31ea8e
Mihai Gaiseanu <Mihai Gaiseanu>
2017-01-10 17:40:45 +0200

    finished the lectures in section 2

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -14,7 +15,55 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() {Name = "Shreck!"};
-            return View(movie);
+            var customers = new List<Customer>()
+            {
+                new Customer() {Name = "Customer 1"},
+                new Customer() {Name = "Customer 2"},
+                new Customer() {Name = "Customer 3"},
+                new Customer() {Name = "Customer 4"},
+                new Customer() {Name = "Customer 5"},
+                new Customer() {Name = "Customer 6"},
+                new Customer() {Name = "Customer 7"}
+            };
+
+
+            /* Not recomended ways of passing data to a view
+            ViewData["Movie"] = movie;
+            ViewBag.Movie = movie;
+            return View();
+            */
+
+            var viewModel = new RandomMovieViewModel() {Movie = movie, Customers = customers};
+
+            return View(viewModel); // recomended way of passing data to a view
+
         }
+
+        // Get: Movies/Edit
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // Get: Movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            int page = pageIndex.HasValue ? pageIndex.Value : 1;
+            string sort = sortBy ?? "name";
+
+            return Content($"pageIndex = {page}; sortBy = {sort}");
+
+        }
+
+        // Get: Movies/ByReleaseDate
+        // Action uses the Route attribute - the recommended way of declaring custom routes
+        // The constraints here are called "atribute route constraints" (ex: range)
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+
     }
 }
