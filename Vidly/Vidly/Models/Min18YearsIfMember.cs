dab2e357d11561f9a470a3a3d709c622320b5ff5
Mihai Gaiseanu <Mihai Gaiseanu>
2017-01-16 17:24:27 +0200

    finished section 5 ('Implementing Validation')

---
+++ Vidly/Vidly/Models/Min18YearsIfMember.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Min18YearsIfMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+            if (customer.MembershipTypeId == MembershipType.Unknown || customer.MembershipTypeId == MembershipType.PayAsYouGo)
+                return ValidationResult.Success;
+
+            if (customer.Birthdate == null)
+                return new ValidationResult("Birthdate is required.");
+
+            var age = DateTime.Today.Year - customer.Birthdate.Value.Year;
+            if (age >= 18)
+                return ValidationResult.Success;
+            else
+                return new ValidationResult("Age must be greater than 18 for membership access");
+        }
+    }
+}
